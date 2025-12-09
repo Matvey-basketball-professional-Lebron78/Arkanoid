5453256d3f7789cf8caeff9cf6a32b4f40df9785
Matveykk2007 <Matveykk2007@mail.ru>
2025-12-09 13:20:13 +0300

    almost done

--- Main.py
+++ Main.py
@@ -17,15 +17,18 @@ t = 0
 FPS = 30
 screen = pygame.display.set_mode((1280, 800))
 finished = False
-font = pygame.font.SysFont('arial', 36, bold=True)
-score_title = font.render('SCORE', True, (255, 255, 255))
+font = pygame.font.SysFont('impact', 80, bold=False)
+font_big = pygame.font.SysFont('arial', 72, bold=True)
+score_title = font.render('SCORE:', True, (255, 255, 255))
 
 #Создание плиток, шарика, платформы
 create_pl() #Это работает!
 platform, ball = start(screen, clock) #Здесь создаются платформа и шарик(всё нормально, они в классах)
 
 #Запуск главного экрана
-
+knopka_x, knopka_y = 
+while mainmenu == True:
+    sc
 #Начало игры(Большая надпись "Начало", затем мячик появляется на верхней части платформы и летит вертикально вверх, но с небольшим углом)
 
 #1) проверка, что еще остались плитки 2) изменение параметров шарика, платформы и плиток 3) отрисовка объектов
@@ -33,7 +36,7 @@ background = load_background('images/background/-1.png')
 while finished == False:
     if score < 78:
         screen.fill('BLACK')
-        screen.blit(score_title, (920, 60))
+        screen.blit(score_title, (920, 50))
         pygame.draw.rect(screen, (255, 255, 255), (47, 50, 816, 695), 5)
         #screen.blit(background, (0, 0))
         mouse_button = pygame.mouse.get_pressed()
@@ -62,7 +65,7 @@ while finished == False:
         platform.draw(screen)
         ball.draw(screen)
         score_value = font.render(str(score), True, (255, 255, 255))
-        screen.blit(score_value, (920, 110))
+        screen.blit(score_value, (920, 150))
         pygame.display.update()
             #FIXME: здесь должна быть функция, рисующая шарик
         for event in pygame.event.get(): #Проверяем, не нажали ли на крестик справа сверху
@@ -70,3 +73,4 @@ while finished == False:
                 finished = True
         clock.tick(50)
     #Здесь будет финальный экран с затраченным временем 1
+pygame.quit()
